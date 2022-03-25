d043de07219862347d2c8fa2bf7e7fc086a105b2
nutpat2539 <nutpat2539@gmail.com>
2022-03-25 10:36:34 +0700

    add cross-env

--- src/lib/knex.js
+++ src/lib/knex.js
@@ -1,5 +1,5 @@
 const knex = require('knex')
 const knexfile = require('./knexfile')
-
+// console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 const db = knex(knexfile.office)
 module.exports = db
